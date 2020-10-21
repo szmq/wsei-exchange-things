87bceb327f9e3f3a06dc9b0d087b5987a72fa037
Szymon Ryczek <szymon.ryczek@sinch.com>
2020-10-21 22:39:58 +0200

    Add Company form

---
+++ Models/CompanyModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace ExchangeThings.Models
+{
+    public class CompanyModel
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public bool IsVisible { get; set; }
+    }
+}
