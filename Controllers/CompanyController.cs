1fdc9710e9fcdb9a79b7cc0b8aea3ca484dcca30
Szymon Ryczek <szymon.ryczek@sinch.com>
2020-10-21 23:00:06 +0200

    Add Company view model

--- Controllers/CompanyController.cs
+++ Controllers/CompanyController.cs
@@ -1,13 +1,28 @@
 ﻿using System;
 using Microsoft.AspNetCore.Mvc;
+using ExchangeThings.Models;
 
 namespace ExchangeThings.Controllers
 {
     public class CompanyController : Controller
     {
+        [HttpGet]
         public IActionResult Index()
         {
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(CompanyModel company)
+        {
+            var viewModel = new CompanyAddedViewModel
+            {
+                NumberOfCharsInName = company.Name.Length,
+                NumberOfCharsInDescription = company.Description.Length,
+                IsHidden = !company.IsVisible
+            };
+
+            return View("CompanyAdded", viewModel);
         }
     }
 }
