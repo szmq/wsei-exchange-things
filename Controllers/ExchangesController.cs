bfd2f90b40a6bea1f826642b5f98cfc202c62e85
Szymon Ryczek <szymon.ryczek@sinch.com>
2020-10-21 21:50:23 +0200

    Add Exchanges controller

---
+++ Controllers/ExchangesController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ExchangeThings.Controllers
+{
+    public class ExchangesController : Controller
+    {
+        //public ExchangesController()
+        //{
+        //}
+
+        public IActionResult Show()
+        {
+            return View();
+        }
+    }
+}
