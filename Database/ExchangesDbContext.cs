4ae1a077935b57d2d35f608176e183e9021c3ed7
Szymon Ryczek <szymon.ryczek@sinch.com>
2020-11-26 19:54:48 +0100

    Add database

---
+++ Database/ExchangesDbContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Wsei.ExchangeThings.Web.Entities;
+
+namespace Wsei.ExchangeThings.Web.Database
+{
+    public class ExchangesDbContext : DbContext
+    {
+        public ExchangesDbContext(DbContextOptions options)
+            : base(options)
+        {
+        }
+
+        public DbSet<ItemEntity> Items { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=localhost,1433; Database=exchangethings;User=SA; Password=admin123!");
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            // fluent configuration ...
+        }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -8,6 +8,8 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Wsei.ExchangeThings.Web.Database;
 
 namespace ExchangeThings
 {
@@ -24,6 +26,10 @@ namespace ExchangeThings
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+
+            services.AddDbContext<ExchangesDbContext>(options => options
+              .UseSqlServer(Configuration.GetConnectionString("ExchangeThings"))
+            );
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
