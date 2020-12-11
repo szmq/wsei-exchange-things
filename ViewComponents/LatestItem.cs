2717fc8f4893df4fd01164f10ab7c1b1b38febca
Szymon Ryczek <szymon.ryczek@sinch.com>
2020-12-11 17:14:59 +0100

    lab razor view

--- ViewComponents/LatestItem.cs
+++ ViewComponents/LatestItem.cs
@@ -5,20 +5,18 @@ using Wsei.ExchangeThings.Web.Database;
 
 namespace Wsei.ExchangeThings.Web.ViewComponents
 {
-    public class LatestItem : ViewComponent
-    {
-        private readonly ExchangesDbContext _dbContext;
-
-        public LatestItem(ExchangesDbContext dbContext)
-        {
-            _dbContext = dbContext;
-        }
-
-        public IViewComponentResult Invoke()
-        {
-            var latestItem = _dbContext.Items.OrderByDescending(x => x.Id).First();
-
-            return View("Index", latestItem);
-        }
-    }
+	public class LatestItem : ViewComponent
+	{
+		private readonly ExchangesDbContext _dbContext;
+		public LatestItem(ExchangesDbContext dbContext)
+		{
+			_dbContext = dbContext;
+		}
+		public IViewComponentResult Invoke()
+		{
+			var latestItem = _dbContext.Items
+			.OrderByDescending(x => x.Id).First();
+			return View("Index", latestItem);
+		}
+	}
 }
